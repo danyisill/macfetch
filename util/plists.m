479b42d53c97030af1b3fecd9f44dfb642f49917
Danny <daniel@danw.website>
2018-12-10 21:10:30 +0300

    refactored

--- util/plists.m
+++ util/plists.m
@@ -1,18 +1,13 @@
 #import <Foundation/Foundation.h>
-char *ver(void){
-	NSDictionary *p = [NSDictionary dictionaryWithContentsOfFile:@"/System/Library/CoreServices/SystemVersion.plist"];
-	const char *version_string = [[p valueForKeyPath:@"ProductVersion"] UTF8String];
-	char *out;
-	int ver = [[[p valueForKeyPath:@"ProductVersion"] componentsSeparatedByString:@"."][1] intValue];
-	char *code[14] = {"Cheetah", "Jaguar", "Panther", "Tiger", "Leopard", "Snow Leopard", "Lion", "Mountain Lion", "Mavericks", "Yosemite", "El Capitan", "Sierra", "High Sierra", "Mojave"};
-	asprintf(&out, "%s %s %s", (ver < 8)?"Mac OS X":(ver < 12)?"OS X":"macOS", version_string, code[ver - 1]);
-	return out;
+char *codenames[14] = {"Cheetah", "Jaguar", "Panther", "Tiger", "Leopard", "Snow Leopard", "Lion", "Mountain Lion", "Mavericks", "Yosemite", "El Capitan", "Sierra", "High Sierra", "Mojave"},
+*accents[9] = {"Graphite", "Red", "Orange", "Yellow", "Green", "", "Purple", "Pink", "Blue"};
+const char *ver(void){
+	NSString *version_string = [[NSDictionary dictionaryWithContentsOfFile:@"/System/Library/CoreServices/SystemVersion.plist"] valueForKeyPath:@"ProductVersion"];
+	int ver = [[version_string componentsSeparatedByString:@"."][1] intValue];
+	return [[NSString stringWithFormat:@"%s %@ %s", (ver < 8)?"Mac OS X":(ver < 12)?"OS X":"macOS", version_string, codenames[ver - 1]] UTF8String];
 }
-char *theme(void){
+const char *theme(void){
 	NSUserDefaults *def = [NSUserDefaults standardUserDefaults];
-	char *out, *accents[9] = {"Graphite", "Red", "Orange", "Yellow", "Green", "", "Purple", "Pink", "Blue"};
-	asprintf(&out, "%s %s",
-	([def stringForKey:@"AppleInterfaceStyle"])?"Dark":"Light",
-	([[[def dictionaryRepresentation] allKeys] containsObject:@"AppleAccentColor"])?accents[[def integerForKey:@"AppleAccentColor"] + 1]:([def integerForKey:@"AppleAquaColorVariant"] == 6)?"Graphite":"Blue");
-	return out;
+	return [[NSString stringWithFormat:@"%s %s", ([def objectForKey:@"AppleInterfaceStyle"])?"Dark":"Light", ([def objectForKey:@"AppleAccentColor"])?accents[[def integerForKey:@"AppleAccentColor"] + 1]:([def integerForKey:@"AppleAquaColorVariant"] == 6)?"Graphite":"Blue"] UTF8String];
 }
+//[[[def dictionaryRepresentation] allKeys] containsObject:@"AppleAccentColor"]
