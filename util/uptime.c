bfe10cb245b0a1e3a94aea86741930c6994a5d13
danyisill <daniel@danw.website>
2018-12-11 00:02:25 +0300

    fix

--- util/uptime.c
+++ util/uptime.c
@@ -15,11 +15,12 @@ char *uptime(void){
 	days = hours / 24;
 	char *out = malloc(64);
 	if(days)
-		len += sprintf(out + len, "%d day%s, ", days, (days == 1)?"":"s");
-	if(hours)
-		len += sprintf(out + len, "%d hour%s, ", hours % 24, (hours % 24 == 1)?"":"s");
-	if(minutes)
-		len += sprintf(out + len, "%d minute%s and ", minutes % 60, (minutes % 60 == 1)?"":"s");
-	len += sprintf(out + len, "%d seconds", seconds % 60);
+		len += sprintf(out + len, "%d day%s%s", days, (days == 1)?"":"s", (hours % 24)?", ":"");
+	if(hours % 24)
+		len += sprintf(out + len, "%d hour%s%s", hours % 24, (hours % 24 == 1)?"":"s", (minutes % 60)?", ":"");
+	if(minutes % 60)
+		len += sprintf(out + len, "%d minute%s%s", minutes % 60, (minutes % 60 == 1)?"":"s", (seconds % 60)?", ":"");
+	if(seconds % 60)
+		len += sprintf(out + len, "%d second%s", seconds % 60, (seconds % 60 == 1)?"":"s");
 	return out;
 }
