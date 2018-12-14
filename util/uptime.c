01ab09823ea738803ad6a5c779da8f0283d8d59f
Danny <daniel@danw.website>
2018-12-14 19:27:52 +0300

    fix

--- util/uptime.c
+++ util/uptime.c
@@ -3,24 +3,26 @@
 #include <sys/sysctl.h>
 #include <math.h>
 #include <stdlib.h>
-
-char *uptime(void){
-	struct timeval boot;
-	size_t size = sizeof(boot),
-	len = 0;
-	sysctlbyname("kern.boottime", &boot, &size, NULL, 0);
-	unsigned seconds = round(difftime(time(NULL), boot.tv_sec)),
-	minutes = seconds / 60,
+char *fmt(unsigned seconds){
+	char *out = malloc(64);
+	size_t len = 0;
+	unsigned minutes = seconds / 60,
 	hours = minutes / 60,
 	days = hours / 24;
-	char *out = malloc(64);
+
 	if(days)
-		len += sprintf(out + len, "%d day%s", days, (days == 1)?"":"s");
+		len += sprintf(out + len, "%dd", days);
 	if(hours % 24)
-		len += sprintf(out + len, ", %d hour%s", hours % 24, (hours % 24 == 1)?"":"s");
+		len += sprintf(out + len, "%dh", hours % 24);
 	if(minutes % 60)
-		len += sprintf(out + len, ", %d minute%s", minutes % 60, (minutes % 60 == 1)?"":"s");
+		len += sprintf(out + len, "%dm", minutes % 60);
 	if(seconds % 60)
-		len += sprintf(out + len, ", %d second%s", seconds % 60, (seconds % 60 == 1)?"":"s");
+		len += sprintf(out + len, "%ds", seconds % 60);
 	return out;
+}
+char *uptime(void){
+	struct timeval boot;
+	size_t size = sizeof(boot);
+	sysctlbyname("kern.boottime", &boot, &size, NULL, 0);
+	return fmt(round(difftime(time(NULL), boot.tv_sec)));
 }
