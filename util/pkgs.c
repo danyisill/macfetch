585c9c14a3b626b8b0e4f6b77f902428be30500a
Larry Bankovich <addaitech@gmail.com>
2023-02-03 22:05:38 -0500

    Add macports to package count

--- util/pkgs.c
+++ util/pkgs.c
@@ -5,7 +5,7 @@
 char *pkgs(void){
 	glob_t gl;
 	size_t total = 0;
-	char *p[] = {"/usr/local/Cellar/*", "/usr/local/Caskroom/*"};
+	char *p[] = {"/usr/local/Cellar/*", "/usr/local/Caskroom/*", "/opt/local/var/macports/software/*"};
 	for(int i = 0; i < (sizeof p / sizeof *p); ++i){
 		glob(p[i], GLOB_NOSORT, NULL, &gl);
 		total += gl.gl_pathc;
