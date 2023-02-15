8f854b1dfc405a4cf2d681416f1b1257c37a6a2d
Larry Bankovich <addaitech@gmail.com>
2023-02-14 19:30:33 -0500

    fix- detect multiple gpus.

--- util/gpu.c
+++ util/gpu.c
@@ -3,24 +3,46 @@
 #include <string.h>
 
 const char *gpu() {
-  char buffer[1024];
-  FILE *fp = popen("system_profiler SPDisplaysDataType | grep \"Chipset Model\"", "r");
+  char buffer[4096]; // Increased buffer size to accommodate larger output
+  FILE *fp = popen("system_profiler SPDisplaysDataType", "r"); // Removed grep filter
   if (fp == NULL) {
     return "N/A";
   }
 
-  fread(buffer, sizeof(char), sizeof(buffer), fp);
+  size_t bytes_read = fread(buffer, sizeof(char), sizeof(buffer), fp);
   pclose(fp);
 
-  char *start = strstr(buffer, ": ") + 2;
-  char *end = strchr(start, '\n');
-  int length = end - start;
-  char result[length + 1];
-  memcpy(result, start, length);
-  result[length] = '\0';
+  char *start = buffer;
+  char *end;
+  char *result = NULL;
 
-  return strdup(result);
+  // Iterate through buffer and search for all instances of "Chipset Model"
+  while ((start = strstr(start, "Chipset Model: ")) != NULL) {
+    start += strlen("Chipset Model: ");
+    end = strchr(start, '\n');
+    int length = end - start;
+    char *model = (char *) malloc(length + 1);
+    memcpy(model, start, length);
+    model[length] = '\0';
+
+    if (result == NULL) {
+      result = model;
+    } else {
+      char *temp = (char *) malloc(strlen(result) + length + 2);
+      sprintf(temp, "%s, %s", result, model);
+      free(result);
+      free(model);
+      result = temp;
+    }
+
+    start = end;
+  }
+
+  if (result == NULL) {
+    return "N/A";
+  } else {
+    return result;
+  }
 }
-
 //GPU detection rewrite
 //author: Larry Bankovich/lennox <addaitech@gmail.com>
